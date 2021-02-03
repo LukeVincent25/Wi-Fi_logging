146e125aa5a610dbc897e917bfba82dc2eb1effd
LukeVincent25 <lv001@uark.edu>
2021-02-03 23:13:45 +0000

    test 2

--- station_log.sh
+++ station_log.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 # this file is to log basic data from a station's connection to an access point
 
+
+
+#-----------------------------
+# Defining Functions
+#-----------------------------
 IsConnected(){
 	
 	r=$(iwconfig | grep 'ESSID') > /dev/null
@@ -16,14 +21,26 @@ IsConnected(){
 }
 
 GetConnectionParameters(){
+	# get ap signal strength
 	command="iw wlan0 station dump | grep signal: "
-	result=$(eval "$command")
-	echo $result
+	signal=$(eval "$command")
+	
+	# get tx bitrate MCS
+	command="iw wlan0 station dump | grep 'tx bitrate:'"
+	txMCS=$(eval "$command")
 }
 
-
-
+#-----------------------------
+# Main Functions
+#-----------------------------
 Main(){
+	#-----------------------------
+	# Setting Variables
+	#-----------------------------
+	fileName='logging_data1'
+	header='Range (m), tx bitrate, signal'
+	
+	#-----------------------------
 	echo "checking if connected to an access point"
 	IsConnected connectionStatus
 	
@@ -33,7 +50,12 @@ Main(){
 	echo "getting connection parameters"
 	GetConnectionParameters
 	
-	echo "this is a csv file" >> logging_data1.csv
+	echo "writing header"
+	sed -i 'Ns\.*\$header\' "./$fileName"
+	
+	echo "writing to log file"
+	echo "$range, $txMCS, $signal" >> "./$fileName"
+	
 }
 
 Main
